f86e892a60a38443b0f7b140002830a750665d3b
noonmap <codnjs8221@naver.com>
2021-09-02 14:28:22 +0900

    Add LA-transformer model and put topdb and LA-tr together
     * add LA-transformer model in personReid dir
     * move top-dropblock dir to personReid dir
     * select reid model to choose 'topdb' or 'la' or 'fake' in run.py

--- configs/runInfo.py
+++ configs/runInfo.py
@@ -1,7 +1,7 @@
 input_video_path = 'data/input/video.webm' # 'testVideo.mp4'
 output_video_path = 'data/output/output_yolov4.avi' #'output_test_final.avi'
 start_frame = 0
-end_frame = 200
+end_frame = 2
 query_image_path = 'tempData/query/' # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
 debug = True
 parallel_processing = True
--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import os, sys
 root_path = os.path.abspath(os.path.dirname(__file__))
 sys.path.append(root_path)
 sys.path.append(root_path + '/deep_sort_yolo4')
-sys.path.append(root_path + '/top-dropblock')
 
 from multiprocessing import Process, Manager
 from configs import runInfo
@@ -12,7 +11,7 @@ from timeit import time
 from demo import detectAndTrack
 from distance import checkDistance
 from collections import Counter # for test(fake reid)
-from main import config_for_topdb, run_top_db_test
+from personReid.personReid import runPersonReid
 from maskdetect.maskDetect import runMaskDetect
 from write_video import writeVideo
 
@@ -21,41 +20,6 @@ output_video_path = runInfo.output_video_path
 start_frame = runInfo.start_frame
 end_frame = runInfo.end_frame
 query_image_path = runInfo.query_image_path
-
-def fakeReid(trackingRslt, reidRslt):
-    # Find most frequent tid(person) in video frames
-    idList = []
-    for aFrameTracking in trackingRslt:
-        for idx, person in enumerate(aFrameTracking):
-            idList.append(person.tid)
-    if len(idList) == 0:
-        print("Nobody in this video: {}".format(input_video_path))
-        print("Tracking result: {}".format(trackingRslt))
-        confirmed_id = -1
-    else:
-        confirmed_id = Counter(idList).most_common(n=1)[0][0]
-    
-    # Fill in the reidRslt
-    for aFrameTracking in trackingRslt:
-        confirmed_idx = -1
-        for idx, person in enumerate(aFrameTracking):
-            if person.tid == confirmed_id:
-                confirmed_idx = idx
-                break
-        reidRslt.append(confirmed_idx)
-
-def personReid(trackingRslt, reidRslt):
-    # CUDA_VISIBLE_DEVICES를 0으로 설정하지 않으면 topdb 돌릴 때 아래와 같은 err가 뜬다
-    # TypeError: forward() missing 1 required positional argument: 'x'
-    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-    top_db_engine, top_db_cfg = config_for_topdb( root_path , query_image_path=query_image_path)
-    run_top_db_test(engine=top_db_engine, cfg=top_db_cfg, 
-                    start_frame=start_frame, end_frame=end_frame,
-                    input_video_path=input_video_path, output_video_path=output_video_path, 
-                    tracking_list=trackingRslt, reid_list=reidRslt, 
-                    query_image_path=query_image_path)
-    # 지금 reidRslt에서 확진자가 없는 경우(-1)는 나오지 않는다. (reid 정확성 문제 때문에)
-
 
 if __name__ == '__main__':
     startTime = time.time()
@@ -68,8 +32,7 @@ if __name__ == '__main__':
         
         # 프로세스 실행 (영상 단위 처리)
         detectTrackProc = Process(target=detectAndTrack, args=(tracking, ))
-        # reidProc = Process(target=fakeReid, args=(tracking, reid))        
-        reidProc = Process(target=personReid, args=(tracking, reid))
+        reidProc = Process(target=runPersonReid, args=(tracking, reid, 'topdb')) # select topdb / la / fake
         distanceProc = Process(target=checkDistance, args=(tracking, reid, distance))
         maskProc = Process(target=runMaskDetect, args=(tracking, reid, distance, mask))
         
