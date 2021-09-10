a2fe4e3b7e6fc9f6a1175c4d2522532e0e63b6e0
noonmap <codnjs8221@naver.com>
2021-09-10 12:42:10 +0900

    Add reid confidence of la and fake1,2 in shm
     * Change to get reid-tid instean reid-personIndex of shm.people[]
     * Add confidence in fake 1,2
     * Change la_transformer.py to get confidence

--- configs/runInfo.py
+++ configs/runInfo.py
@@ -5,7 +5,7 @@ input_video_path = 'data/input/08_14_2020_1_1.mp4'
 output_video_path = 'data/output/08_14_2020_1_1.avi'
 output_json_path = 'data/output/output.json' 
 start_frame = 750
-end_frame = 770
+end_frame = 760
 query_image_path = 'tempData/query/' # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
 
 # log setting 
@@ -21,7 +21,7 @@ parallel_processing = False
 use_mask_voting = False 
 
 # reid model setting
-reid_model = 'topdb' # 'fake2' / 'fake' / 'topdb' / 'la'
+reid_model = 'fake2' # 'fake2' / 'fake' / 'topdb' / 'la'
 
 # allocate specific gpu device
 trackingGPU = 0
