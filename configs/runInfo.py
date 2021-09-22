4e7ae1445a4ed8577cacc30dbcbb3fbb6349fe53
zetwhite <zetwthie@naver.com>
2021-09-23 01:51:05 +0900

    mask accuracy : add mask accuracy check shell script for scene 1

--- configs/runInfo.py
+++ configs/runInfo.py
@@ -2,25 +2,26 @@
 import logging
 
 # anaylsis time, file setting 
-input_video_path        = "proj/data/input/08_14_2020_1_1.mp4"
-query_image_path        = "proj/data/input/query/" # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
-output_video_path       = "proj/data/output/08_14_2020_1_1.avi"
-output_json_path        = "proj/data/output/analysis/08_14_2020_1_1.json"
-output_contactors_path  = "proj/data/output/analysis/" 
-start_frame             = 800
-end_frame               = 810
+input_video_path = 'accuracy_check/test_video_data/08_14_2020_1_1.mp4'
+query_image_path = "data/input/query/" # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
+output_video_path = "data/output/08_14_2020_1_1.avi"
+output_json_path = "data/output/analysis/08_14_2020_1_1.json"
+output_contactors_path  = "data/output/analysis/" 
+start_frame = 0
+end_frame = 100000
 
 # log setting 
-logfile_name            = "log.txt"
-console_log_level       = 20 # select in [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL] 
-file_log_level          = 10
+logfile_name = "log.txt"
+console_log_level = 20 # select in [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL] 
+file_log_level = 10
 
 # accuracy check setting
-write_result            = False
+write_result = False
 
 # system setting 
-parallel_processing     = True
-use_mask_voting         = False 
+parallel_processing = True
+use_mask_voting = True
+use_reid_voting = True
 
 # reid model setting
 reid_model              = 'topdb' # 'fake2' / 'fake' / 'topdb' / 'la'
