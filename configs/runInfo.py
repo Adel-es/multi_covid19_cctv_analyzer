b9c29e2521616a2646ba98b69dd892d6c9ef20ca
zetwhite <zetwthie@naver.com>
2021-09-24 17:56:25 +0900

    fix find auto end frame

--- configs/runInfo.py
+++ configs/runInfo.py
@@ -1,14 +1,21 @@
 
 import logging
-
+    
 # anaylsis time, file setting 
-input_video_path = 'accuracy_check/test_video_data/08_14_2020_1_1.mp4'
+input_video_path = 'accuracy_check/test_video_data/08_14_2020_4_1.mp4'
 query_image_path = "data/input/query/" # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
-output_video_path = "data/output/08_14_2020_1_1.avi"
+output_video_path = 'tmp.avi'
 output_json_path = "data/output/analysis/08_14_2020_1_1.json"
 output_contactors_path  = "data/output/analysis/" 
 start_frame = 0
-end_frame = 100000
+end_frame = -1
+
+if end_frame == -1 : 
+    print("end frame is updated!")
+    import cv2 
+    video_capture = cv2.VideoCapture(input_video_path)
+    real_end_frame = int(video_capture.get( cv2.CAP_PROP_FRAME_COUNT )) - 1
+    end_frame = real_end_frame
 
 # log setting 
 logfile_name = "log.txt"
