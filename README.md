98e87118e895b621314b9c6d76247911e885c6db
fridaySong3 <pleasure5567@gmail.com>
2021-10-18 18:31:50 +0900

    Add information to README.md, annotate runInfo.py and add environment.yml

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
-# 실행 방법
+# System Structure
+![system_structure](./for_readme/system_structure.jpg)
+# System operstion
+![window1](./for_readme/window1.jpg)
+![window2-1](./for_readme/window2-1.jpg)
+![window2-2](./for_readme/window2-2.jpg)
+![window3-1](./for_readme/window3-1.jpg)
+![window3-2](./for_readme/window3-2.jpg)
+![window4](./for_readme/window4.jpg)
+![window5](./for_readme/window5.jpg)
+![window6-1](./for_readme/window6-1.jpg)
+![window6-2](./for_readme/window6-2.jpg)
+
+아직 Person re-identification의 정확도가 높지 못하다.
+# Getting Start
+## Requirements
+-conda 4.10.1
 ## Setting
 아래를 순서대로 실행한다.
+```
+$ conda env create -f environment.yml
+```
 ```
 $ cd deep-sort-yolo4
 $ ./model_data/download_weights.sh
@@ -15,25 +34,23 @@ $ cd personReid/LA_Transformer
 $ ./download_weights.sh
 ```
 ## Optional Setting
-`deep-sort-yolo4/demo.py` 에서 세부 설정 값을 변경할 수 있다.
+`configs/runInfo.py`에서 세부 설정 값을 변경할 수 있다.
 - `input_video_path` : 분석할 입력 영상 경로
 - `output_video_path` : 출력 영상 경로
 - `start_frame` : 입력 영상에서 분석을 시작할 frame 시점
 - `end_frame` : 입력 영상에서 분석을 종료할 frame 시점
 - `query_image_path` : 확진자의 사진을 넣을 디렉토리 경로
 ## Execution
+UI를 사용하여 실행하고 싶을 경우 아래와 같이 실행한다.
+```
+$ python application.py
+```
+UI 없이 `configs/runInfo.py`의 설정대로 실행하여 출력 영상 및 파일만 생성하고 싶을 경우 아래와 같이 실행한다.
 ```
 $ python run.py
 ```
-
-# Deep-SORT-YOLOv4
-referenced from https://github.com/LeonLok/Deep-SORT-YOLOv4
-
-# top-dropblock
-referenced from https://github.com/RQuispeC/top-dropblock
-
-# LA-transformer
-referenced from https://github.com/SiddhantKapil/LA-Transformer
-
-# Calculate AP
-referenced from https://github.com/herbwood/mAP
+# References
+- Deep-SORT-YOLOv4: https://github.com/LeonLok/Deep-SORT-YOLOv4
+- top-dropblock: https://github.com/RQuispeC/top-dropblock
+- LA-transformer: https://github.com/SiddhantKapil/LA-Transformer
+- AP calculation: https://github.com/herbwood/mAP
--- configs/runInfo.py
+++ configs/runInfo.py
@@ -2,16 +2,15 @@
 import logging
     
 # anaylsis time, file setting 
-input_video_path        = "proj/data/input/08_14_2020_1_1.mp4"
-query_image_path        = "proj/data/input/query/" # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
-output_video_path       = "proj/data/output/08_14_2020_1_1.avi"
-output_json_path        = "proj/data/output/analysis/08_14_2020_1_1.json"
-output_contactors_path  = "proj/data/output/analysis/" 
+input_video_path = 'data/input/08_14_2020_3_1.mp4'
+query_image_path = "data/input/query/" # query image의 이름은 "숫자_숫자_숫자" 로 설정. ex) 1_0_0.jpg
+output_video_path = 'data/output/08_14_2020_3_1.avi'
+output_json_path = "data/output/analysis/08_14_2020_3_1.json"
+output_contactors_path  = "data/output/analysis/"  # Save path of images displayed on contactor list
 start_frame = 0
-end_frame = -1
+end_frame = -1  # If end_frame is -1, run to the end of the video
 
 if end_frame == -1 : 
-    print("end frame is updated!")
     import cv2 
     video_capture = cv2.VideoCapture(input_video_path)
     real_end_frame = int(video_capture.get( cv2.CAP_PROP_FRAME_COUNT )) - 1
@@ -20,18 +19,18 @@ if end_frame == -1 :
 # log setting 
 logfile_name = "log.txt"
 console_log_level = logging.WARNING # select in [logging.DEBUG, logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL] 
-file_log_level = 10
+file_log_level = logging.DEBUG
 
 # create output.json or not 
 write_result = False
 
 # system setting 
-parallel_processing     = True
-use_mask_voting = False
-use_reid_voting = False
+parallel_processing     = True  # If false, run serially
+use_mask_voting         = True
+use_reid_voting         = True
 
 # reid model setting
-reid_model              = 'topdb' # 'fake2' / 'fake' / 'topdb' / 'la'
+reid_model              = 'topdb' # 'topdb' / 'la' (and for test 'fake' / 'fake2')
 reid_threshold          = 0.975
 
 # allocate specific gpu device
